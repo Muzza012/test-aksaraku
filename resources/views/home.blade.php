cd170d14cb6a41cd82e9ae7cecd1f94bdf44b101
M. Muzzainur Rahman <mmuzzainurrahman@gmail.com>
2026-01-06 19:58:41 +0700

    Add JavaScript file to Vite configuration in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - AksaraKu</title>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
